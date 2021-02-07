819e98a00b355568d6b70a31368a8002f632ee6b
akshayboddhul <akshayboddhul45@gmail.com>
2021-02-07 17:20:27 +0530

    Updated the deisgn UI

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,29 @@
-let amount = document.getElementById('amt')
-const calBtn = document.getElementById('cal')
-const clearBtn = document.getElementById('clear')
+let jar_arr = ['necesities', 'long-term','play','education','financial','charity']
 
-let total = document.getElementById('total-amt')
+let jar1Label = document.getElementById('jar1')
+let jar2Label = document.getElementById('jar2')
+let jar3Label = document.getElementById('jar3')
+let jar4Label = document.getElementById('jar4')
+let jar5Label = document.getElementById('jar5')
+let jar6Label = document.getElementById('jar6')
 
-let jar1Label = document.getElementById('jar1-amt')
-let jar2Label = document.getElementById('jar2-amt')
-let jar3Label = document.getElementById('jar3-amt')
-let jar4Label = document.getElementById('jar4-amt')
-let jar5Label = document.getElementById('jar5-amt')
-let jar6Label = document.getElementById('jar6-amt')
+let amt = document.getElementById('amount')
+
+amt.addEventListener('keyup', () => {
+    const jar1 = calculateMoney1(amount)
+    const jar2 = calculateMoney2(amount)
+    const jar3 = calculateMoney2(amount)
+    const jar4 = calculateMoney2(amount)
+    const jar5 = calculateMoney2(amount)
+    const jar6 = calculateMoney3(amount)
+
+    jar1Label.innerHTML = jar1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
+    jar2Label.innerHTML = jar2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
+    jar3Label.innerHTML = jar3.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
+    jar4Label.innerHTML = jar4.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
+    jar5Label.innerHTML = jar5.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
+    jar6Label.innerHTML = jar6.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
+})
 
 
 function calculateMoney1(amount){
@@ -21,35 +35,3 @@ function calculateMoney2(amount){
 function calculateMoney3(amount){
     return  (amount.value * 05 ) / 100
 }
-
-amount.addEventListener('keyup', () => {
-        const jar1 = calculateMoney1(amount)
-        const jar2 = calculateMoney2(amount)
-        const jar3 = calculateMoney2(amount)
-        const jar4 = calculateMoney2(amount)
-        const jar5 = calculateMoney2(amount)
-        const jar6 = calculateMoney3(amount)
-        // x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        jar1Label.innerHTML = jar1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
-        jar2Label.innerHTML = jar2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
-        jar3Label.innerHTML = jar3.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
-        jar4Label.innerHTML = jar4.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
-        jar5Label.innerHTML = jar5.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
-        jar6Label.innerHTML = jar6.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-"
-    
-        let sum = jar1 + jar2 + jar3 + jar4 + jar5 + jar6
-        
-        total.innerHTML = (sum.toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "/-" 
-})
-
-
-clearBtn.addEventListener('click', () => {
-    jar1Label.innerText = ''
-    jar2Label.innerText = ''
-    jar3Label.innerText = ''
-    jar4Label.innerText = ''
-    jar5Label.innerText = ''
-    jar6Label.innerText = ''
-    total.innerText = ''
-    amount.value = ''
-})
